7cb838a429214710637d67ee1d72bea516559603
Ayush Agarwal <56976274+ayushagarwal693@users.noreply.github.com>
2021-03-25 21:57:07 +0530

    Create 13. Smallest Positive missing number.cpp

---
+++ ppc/Arrays/13. Smallest Positive missing number.cpp
@@ -0,0 +1,56 @@
+Given an array arr[] of size N, find the smallest positive number missing from the array.
+
+ 
+
+Example 1:
+
+Input:
+N = 5
+arr[] = {1,2,3,4,5}
+Output: 6
+Explanation: Smallest positive missing
+number is 6.
+ 
+
+Example 2:
+
+Input:
+N = 5
+arr[] = {0,-10,1,3,-20}
+Output: 2
+ 
+
+Your Task:
+You don't need to read input or print anything. The task is to complete the function findMissing() which takes arr and N as input parameters and returns the smallest positive missing number.
+
+ 
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+ 
+
+Constraints:
+1 <= N <= 106
+-106 <= arr[i] <= 106
+  
+  /*=================================================================================================================*/
+  
+  
+  int findMissing(int arr[], int n) { 
+        
+        const int N = 1e6 + 2;
+        bool check[N];
+        for(int i=0;i<N;i++)
+            check[i] = false;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>=0)
+                check[arr[i]] = true;
+        }
+        for(int i=1;i<N;i++)
+        {
+            if(check[i]==false)
+            return i;
+        }
+    }
