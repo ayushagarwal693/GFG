2a9378e226779608784ba806cb6c8a36f28542d2
Ayush Agarwal <56976274+ayushagarwal693@users.noreply.github.com>
2021-03-25 15:54:29 +0530

    Create 4. Who has the majority?.cpp

---
+++ ppc/Arrays/4. Who has the majority?.cpp
@@ -0,0 +1,60 @@
+Given an array arr[] of size N and two elements x and y, use counter variables to find which element appears most in the array, x or y. If both elements have the same frequency, then return the smaller element.
+Note:  We need to return the element, not its count.
+
+ 
+
+Example 1:
+
+Input:
+N = 11
+arr[] = {1,1,2,2,3,3,4,4,4,4,5}
+x = 4, y = 5
+Output: 4
+Explanation: 
+frequency of 4 is 4.
+frequency of 5 is 1.
+ 
+
+Example 2:
+
+Input:
+N = 8
+arr[] = {1,2,3,4,5,6,7,8}
+x = 1, y = 7
+Output: 1
+Explanation: 
+frequency of 1 is 1.
+frequency of 7 is 1.
+Since 1 < 7, return 1.
+ 
+
+Your Task:
+You don't need to read input or print anything. Complete the function majorityWins() that takes array, n, x, y as input parameters and return the element with higher frequency.
+
+ 
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+ 
+
+Constraints:
+1 <= n <= 103
+0 <= arri , x , y <= 108
+
+ /*==========================================================================*/
+  
+  int majorityWins(int arr[], int n, int x, int y) {
+        int count_x = 0, count_y = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i] == x)
+            count_x++;
+            else if(arr[i] == y)
+            count_y++;
+        }
+        if(count_x == count_y)
+            return (x<y)?x:y ;
+        else
+            return (count_x > count_y)?x:y ;
+    }
