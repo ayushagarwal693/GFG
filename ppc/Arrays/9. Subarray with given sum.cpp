bffcf5cceecdb75c7e4090565a2313afc97224d4
Ayush Agarwal <56976274+ayushagarwal693@users.noreply.github.com>
2021-03-26 21:51:26 +0530

    Create 9. Subarray with given sum.cpp

---
+++ ppc/Arrays/9. Subarray with given sum.cpp
@@ -0,0 +1,76 @@
+Given an unsorted array A of size N that contains only non-negative integers, find a continuous sub-array which adds to a given number S.
+
+ 
+
+Example 1:
+
+Input:
+N = 5, S = 12
+A[] = {1,2,3,7,5}
+Output: 2 4
+Explanation: The sum of elements 
+from 2nd position to 4th position 
+is 12.
+ 
+
+Example 2:
+
+Input:
+N = 10, S = 15
+A[] = {1,2,3,4,5,6,7,8,9,10}
+Output: 1 5
+Explanation: The sum of elements 
+from 1st position to 5th position
+is 15.
+ 
+
+Your Task:
+You don't need to read input or print anything. The task is to complete the function subarraySum() which takes arr, N and S as input parameters and returns a list containing the starting and ending positions of the first such occurring subarray from the left where sum equals to S. The two indexes in the list should be according to 1-based indexing. If no such subarray is found, return -1. 
+
+ 
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+ 
+
+Constraints:
+1 <= N <= 105
+1 <= Ai <= 1010
+  
+  /*===============================================================================================*/
+  
+  
+  vector<int> subarraySum(int arr[], int n, int sum)
+    {
+        vector<int> res(2, -1);
+        int curr_sum = arr[0], start = 0, i;
+
+    /* Add elements one by one to curr_sum and 
+    if the curr_sum exceeds the sum,
+    then remove starting element */
+    for (i = 1; i <= n; i++) 
+    {
+        // If curr_sum exceeds the sum,
+        // then remove the starting elements
+        while (curr_sum > sum && start < i - 1) {
+            curr_sum = curr_sum - arr[start];
+            start++;
+        }
+
+        // If curr_sum becomes equal to sum,
+        // then return true
+        if (curr_sum == sum) {
+            res[0] = start+1;
+            res[1] = i;
+            return res;
+        }
+
+        // Add this element to curr_sum
+        if (i < n)
+            curr_sum = curr_sum + arr[i];
+    }
+
+        res.erase(res.begin() + 1);
+        return res;
+    }
