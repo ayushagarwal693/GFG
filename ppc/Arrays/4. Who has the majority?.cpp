ef62f23228f313dbb146f7688b54cfcae1cdc9e0
Ayush Agarwal <56976274+ayushagarwal693@users.noreply.github.com>
2021-03-28 13:44:41 +0530

    Update 4. Who has the majority?.cpp

--- ppc/Arrays/4. Who has the majority?.cpp
+++ ppc/Arrays/4. Who has the majority?.cpp
@@ -1,3 +1,7 @@
+/*
+Who has the majority?
+=====================
+
 Given an array arr[] of size N and two elements x and y, use counter variables to find which element appears most in the array, x or y. If both elements have the same frequency, then return the smaller element.
 Note:  We need to return the element, not its count.
 
@@ -42,7 +46,8 @@ Constraints:
 1 <= n <= 103
 0 <= arri , x , y <= 108
 
- /*==========================================================================*/
+*/
+
   
   int majorityWins(int arr[], int n, int x, int y) {
         int count_x = 0, count_y = 0;
